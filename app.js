811748657e27d67c88a5d6d0fa92268bd89c8224
danutibe07 <danielutibe07@gmail.com>
2023-02-04 21:00:45 +0100

    modified: app.js

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-const handburger = document.querySelector(".handburger")
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
 const navMenu = document.querySelector(".nav-menu")
 
-handburger.addEventListener("click", () =>{
-    handburger.classList.toggle("active")
+menuBtn.addEventListener("click", () =>{
+    menuBtn.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
 
+navMenu.addEventListener("click", ()=>{
+    navMenu.classList.toggle("active")
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+      } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+      }    
+})
+
+
 const form = document.getElementById('form');
-const fullname = document.getElementById('fullname');
+const username = document.getElementById('username');
 const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -42,6 +67,8 @@ const isValidEmail = email => {
 const validateInputs = () => {
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
 
     if(usernameValue === '') {
         setError(username, 'Username is required');
@@ -56,7 +83,25 @@ const validateInputs = () => {
     } else {
         setSuccess(email);
     }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
 };
+
 
 
 
