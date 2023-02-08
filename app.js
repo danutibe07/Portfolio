c2d8d4e9b0808afc539727bc0db1202417f0627a
danutibe07 <danielutibe07@gmail.com>
2023-02-08 18:09:39 +0100

    first part of modal popup menu for works section

--- app.js
+++ app.js
@@ -28,21 +28,49 @@ navMenu.addEventListener("click", () => {
   }
 });
 
-// localStorage
-let submitBtn = document.getElementById("submit");
-let fullName = document.getElementById("fullname");
-let email = document.getElementById("email");
-let message = document.getElementById("message");
+// // localStorage
+// let submitBtn = document.getElementById("submit");
+// let fullName = document.getElementById("fullname");
+// let email = document.getElementById("email");
+// let message = document.getElementById("message");
 
-submitBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  fullName = fullName.value;
-  localStorage.setItem("Full Name", fullName);
-  email = email.value;
-  localStorage.setItem("Email", email);
-  message = message.value;
-  localStorage.setItem("message", message);
-//   validateInputs();
-  document.getElementById("form").reset();
-});
+// function validateForm() {
+//   if (fullName.value == "" , email.value == "" , message.value = "") {
+//     alert("Name must be filled out");
+//   }
+//   else {
+//     alert("go through")
+//   }
+// }
+// submitBtn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   fullName = fullName.value;
+//   localStorage.setItem("Full Name", fullName);
+//   email = email.value;
+//   localStorage.setItem("Email", email);
+//   message = message.value;
+//   localStorage.setItem("message", message);
+//    validateForm();
+//   document.getElementById("form").reset();
+// });
+
+// let modal1 = document.querySelector(".modal1")
+// let modal2 = document.querySelector(".modal2")
+
+// let modalBtn = document.querySelector("#project")
+// let span = document.getElementsByClassName("close")[0];
+
+// modalBtn.addEventListener("click", () => {
+//   modal.style.display = "block" ;
+// });
+
+// span.onclick = function() {
+//   modal.style.display = "none" ;
+// }
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none" ;
+//   }
+// }
 
